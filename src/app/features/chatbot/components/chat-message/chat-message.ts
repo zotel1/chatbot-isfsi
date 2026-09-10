24af487907d42c2bd249128a75b855d4dc6db2b3
zotel1 <zotelsigel@gmail.com>
2026-09-10 03:29:45 -0300

    adaptamos las pantallas

--- src/app/features/chatbot/components/chat-message/chat-message.ts
+++ src/app/features/chatbot/components/chat-message/chat-message.ts
@@ -1,21 +1,47 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+} from '@angular/core';
 
-import { ChatMessage as ChatMessageModel } from '../../models/chat-message.model';
+import {
+  marked,
+} from 'marked';
 
-import { BotAvatar } from '../bot-avatar/bot-avatar';
-import { SourceList } from '../source-list/source-list';
+import {
+  ChatMessage as ChatMessageModel,
+} from '../../models/chat-message.model';
+
+import {
+  BotAvatar,
+} from '../bot-avatar/bot-avatar';
+
+import {
+  SourceList,
+} from '../source-list/source-list';
 
 @Component({
   selector: 'app-chat-message',
+
   standalone: true,
+
   imports: [
     BotAvatar,
     SourceList,
   ],
+
   templateUrl: './chat-message.html',
+
   styleUrl: './chat-message.css',
 })
 export class ChatMessage {
+
   @Input({ required: true })
   message!: ChatMessageModel;
+
+  get renderedText(): string {
+
+    return marked.parse(
+      this.message.text
+    ) as string;
+  }
 }
