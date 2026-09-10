278ad113caa7dc98b76e1c8011be776e338bcb4a
zotel1 <zotelsigel@gmail.com>
2026-09-09 21:25:38 -0300

    comenzamos con la estructura del chatbot

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.css',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.html',
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('chatbot-isf');
-}
+export class App {}
